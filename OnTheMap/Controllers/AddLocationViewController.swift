46e246c42c9ae7d727eea3b08bbc346d16cd8906
José Naves <josenaves@gmail.com>
2019-03-18 09:07:05 -0300

    feat: Add views

---
+++ OnTheMap/Controllers/AddLocationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by José Naves Moura Neto on 18/03/19.
+//  Copyright © 2019 José Naves Moura Neto. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class AddLocationViewController : UIViewController {
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var linkTextField: UITextField!
+    
+    @IBOutlet weak var locationButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
---
+++ OnTheMap/Controllers/LocationDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDetailViewController.swift
+//  OnTheMap
+//
+//  Created by José Naves Moura Neto on 18/03/19.
+//  Copyright © 2019 José Naves Moura Neto. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationDetailViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var buttonFinish: UIButton!
+    
+}
