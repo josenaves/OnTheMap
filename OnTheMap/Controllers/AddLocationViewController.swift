9054a322b92b875ec5d07b98e6befa9068ccc434
José Naves <josenaves@gmail.com>
2019-03-19 21:58:02 -0300

    feat: Search user entered location

--- OnTheMap/Controllers/AddLocationViewController.swift
+++ OnTheMap/Controllers/AddLocationViewController.swift
@@ -18,4 +18,52 @@ class AddLocationViewController : UIViewController {
     @IBOutlet weak var locationButton: UIButton!
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var searchedPlacemark: MKPlacemark?
+    
+    private let SEGUE = "addLocationSegue"
+    
+    @IBAction func findLocation(_ sender: UIButton!) {
+        print("Find location")
+        
+        guard let location = locationTextField.text, !location.isEmpty,
+              let linkText = linkTextField.text,     !linkText.isEmpty else {
+                    
+            print("Fill ALL FIELDS !")
+            return
+        }
+        
+        sender.isEnabled = false
+        
+        let locationSearchRequest = MKLocalSearch.Request()
+        locationSearchRequest.naturalLanguageQuery = location
+        
+        locationButton.isEnabled = false
+        activityIndicator.startAnimating()
+        
+        let localSearch = MKLocalSearch(request: locationSearchRequest)
+        localSearch.start { response, error in
+            self.activityIndicator.stopAnimating()
+            self.locationButton.isEnabled = true
+            
+            guard error == nil, let response = response, !response.mapItems.isEmpty else {
+                self.presentAlert(withTitle: "Error", message: "Location not found, try something different!")
+                return
+            }
+            
+            /// The placemark searched by the user
+            let firstResult = response.mapItems.first!
+            
+            print("firstResult: \(firstResult)")
+            print("Coordinates: \(firstResult.placemark.coordinate)")
+            print("countryCode: \(firstResult.placemark.countryCode ?? "?")")
+            print("title: \(firstResult.placemark.title ?? "?")")
+            print("name: \(firstResult.name ?? "?")")
+            
+            self.searchedPlacemark = response.mapItems.first!.placemark
+            self.performSegue(withIdentifier: self.SEGUE, sender: self)
+        }
+        
+        activityIndicator.stopAnimating()
+    }
 }
