963209e929df3d7859e2c891b885dd56a56f0c0a
José Naves <josenaves@gmail.com>
2019-03-26 12:01:50 -0300

    fix: Avoid soft keyboard to cover textfields

--- OnTheMap/Controllers/AddLocationViewController.swift
+++ OnTheMap/Controllers/AddLocationViewController.swift
@@ -40,8 +40,24 @@ class AddLocationViewController : UIViewController {
         }
         locationTextField.text = ""
         linkTextField.text = ""
-    }
 
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
     
     @IBAction func findLocation(_ sender: UIButton?) {
         
